68438f38da1369772086ded6f833a540c304c7d9
Sahan Perera <sahan.2013040@iit.ac.lk>
2017-01-10 13:55:48 +0530

    Make View Responsive
    Show-hide elements according to mobile size using bootstrap, Added Jquery 1.4.5 min file

--- application/views/reservation_view.php
+++ application/views/reservation_view.php
@@ -6,7 +6,7 @@
       <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
       <link rel="stylesheet" type="text/css" href="../css/style.css">
       <script src="../js/jquery-3.1.1.min.js"></script>
-      <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+      <script src="../js/jquery.mobile-1.4.5.min.js"></script>
       <script src="../js/bootstrap.js"></script>
       <script type="application/javascript">
       $( document ).ready(function() {
@@ -14,59 +14,58 @@
          var isiPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
          if (isiPad){
              console.log("ipad ");
-             $('#i').text('ipad');
-             $('#iphoneM').hide();
          } else if (isiPhone){
              console.log("iphone ");
-             $('#i').text('ipad');
-              $('#ipadM').hide();
          } else {
              console.log("another device");
-             $('#i').text('ipad');
-             $('#iphoneM').hide();
          }
         });
       </script>   
    </head>
    <body>
+   <img src="../images/logo_iPhone.png" width="100%" height="80%">
 
-      <div id="ipadM" class="menu opened">
-         <br>
-         <img src="../images/logo_iPhone.png" width="100%" height="10%"> <br> <br>
-         <img src="../images/user.png" id="userImg" width="50%" height="30%">
-         <label class="username"> John Snow </label>
-         <ul>
-            <li> <button class="btn btn-secondary btn-lg btn-block menubutton" >
-               <em id="ctBtnL" class="leftImage"> </em> Create Treasure Hunt <!-- <em id="ctBtnR" class='rightImage'></em> --> </button> 
-            </li>
-            <li> <button class="btn btn-secondary btn-lg btn-block menubutton" >
-               <em id="stBtnL" class="leftImage"> </em> Start Treasure Hunt <!-- <em class='rightImage'> --></em> </button> 
-            </li>
-            <li> <button class="btn btn-secondary btn-lg btn-block menubutton" >
-               <em id="profileBtnL" class="leftImage"> </em> Profile <!-- <em class='rightImage'></em> --> </button> 
-            </li>
-            <li> <button class="btn btn-secondary btn-lg btn-block menubutton" >
-               <em id="helpBtnL" class="leftImage"> </em> Help <!-- <em class='rightImage'> --></em> </button> 
-            </li>
-            </li>
-         </ul>
-      </div>
-
-    <div id="iphoneM" style="padding-left:2px; padding-right: 2px;">
-      <table width="90%" height="20%" cellspacing="0" cellpadding="0" border="0" id="Table_01">
-      <tbody><tr><td><a href="URL"><img width="60%" height="14.5%" alt="" src="../images/ct.png" /></a></td>
-      <td><a href="URL"><img width="60%" height="15%" alt="" src="../images/st.png" /></a></td>
-      <td><a href="URL"><img width="60%" height="15%" alt="" src="../images/person.png" /></a></td>
-      <td><a href="URL"><img width="250%" height="65%" alt="" src="../images/help.png" /></a></td>
-      </tr>
-      </tbody></table>
+   <div id="ipadM" class="menu opened container-fluid visible-md hiden-xs" style="margin-top: 30px">
+      <br>
+      <img src="../images/user.png" id="userImg" width="50%" height="30%">
+      <label class="username"> John Snow </label>
+      <ul>
+         <li>
+            <button class="btn btn-secondary btn-lg btn-block menubutton">
+               <em id="ctBtnL" class="leftImage"> </em> Create Treasure Hunt
+               <!-- <em id="ctBtnR" class='rightImage'></em> --> </button>
+         </li>
+         <li>
+            <button class="btn btn-secondary btn-lg btn-block menubutton">
+               <em id="stBtnL" class="leftImage"> </em> Start Treasure Hunt <!-- <em class='rightImage'> --></em>
+            </button>
+         </li>
+         <li>
+            <button class="btn btn-secondary btn-lg btn-block menubutton">
+               <em id="profileBtnL" class="leftImage"> </em> Profile <!-- <em class='rightImage'></em> --> </button>
+         </li>
+         <li>
+            <button class="btn btn-secondary btn-lg btn-block menubutton">
+               <em id="helpBtnL" class="leftImage"> </em> Help <!-- <em class='rightImage'> --></em> </button>
+         </li>
+         </li>
+      </ul>
    </div>
 
-      <div id="content" class="text" style="padding-left:15%; padding-top:5%;">
+   <div id="iphoneM" class="container-fluid visible-xs hiden-md">
+       <table width="90%" cellspacing="0" cellpadding="0" border="0" id="Table_01">
+          <tr>
+             <td><a href="#"><img width="60%" height="14.5%" alt="" src="../images/ct.png"/></a></td>
+             <td><a href="#"><img width="60%" height="15%" alt="" src="../images/st.png"/></a></td>
+             <td><a href="#"><img width="60%" height="15%" alt="" src="../images/person.png"/></a></td>
+             <td><a href="#"><img width="250%" height="65%" alt="" src="../images/help.png"/></a></td>
+          </tr>
+       </table>
+   </div>
+
+      <div id="content" class="container-fluid col-md-8 col-md-push-4">
          <div>
-            <label class="col-md-12">
-               <h2 class="reservationTitle"> Reservation </h2>
-            </label>
+               <h2 class="reservationTitle col-centered"> Reservation </h2>
          </div>
          <br>
          <div class="form-group row">
@@ -122,6 +121,5 @@
               </div>
             </div>
          </div>
-      </div>
       </div>
    </body>
