a62ba60c4055f8040690fb07ea66725f315147aa
Tharindu <darshana20124@live.com>
2017-01-13 04:27:34 +0530

    sign in backend

---
+++ application/models/userModel.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class userModel extends CI_Model {
+
+    public function __construct() {
+        // Call the CI_Model constructor
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function createUser($email, $username, $password)
+    {
+        $query = $this->db->query("SELECT * FROM users where email='".$email."'");
+        if ($query->num_rows() > 0) {
+            return false;
+        }
+        else{
+            $data = array('username' => $username, 'password' => $password, 'email' => $email);
+            $this->db->insert('users', $data);
+        }
+
+    }
+
+    public function signIn($username, $password)
+    {
+        $query = $this->db->query("SELECT * FROM users where username='".$username."' AND password ='".$password."'");
+        if ($query->num_rows() > 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
