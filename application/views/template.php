2cc34e41d93f5272c7d41181c463130d3808ebd4
Sahan Perera <sahan.2013040@iit.ac.lk>
2017-01-12 05:37:52 +0530

    Help page button toggle
    Image toggle for Help page

--- application/views/template.php
+++ application/views/template.php
@@ -2,18 +2,40 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../../css/style.css">
-    <script src="../../js/jquery-3.1.1.min.js"></script>
-    <script src="../../js/jquery.mobile-1.4.5.min.js"></script>
-    <script src="../../js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="/goc/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/goc/css/style.css">
+    <script src="/goc/js/jquery-3.1.1.min.js"></script>
+    <script src="/goc/js/jquery.mobile-1.4.5.min.js"></script>
+    <script src="/goc/js/bootstrap.js"></script>
+    <script type="application/javascript">
+        $(document).ready(function () {
+
+            $('#createTH_Btn').click();
+            $('#startTH_Btn').click();
+            $('#profile_Btn').click();
+            $('#help_Btn').click(function () {
+                window.location = '<?php echo base_url().'index.php/menuController/help'  ?>';
+
+            });
+
+
+            $('#help_Btn').click(function () {
+                console.log('okkkokokok');
+                if ($(this).attr("src") == "/goc/images/menu_buttons/flathelp.png")
+                    $(this).attr("src", "/goc/images/menu_buttons/darkhelp.png");
+                else
+                    $(this).attr("src", "/goc/images/menu_buttons/flathelp.png");
+            });
+
+        });
+    </script>
 </head>
 <body>
-<img src="../../images/logo_iPhone.png" width="100%" height="80%">
+<img src="/goc/images/logo_iPhone.png" width="100%" height="80%">
 
 <div id="ipadM" class="menu opened container-fluid visible-md hidden-xs" style="margin-top: 30px">
     <br>
-    <img src="../../images/user.png" id="userImg" width="50%" height="30%">
+    <img src="/goc/images/user.png" id="userImg" width="50%" height="30%">
     <label class="username"> John Snow </label>
     <ul>
         <li>
@@ -40,10 +62,14 @@
 <div id="iphoneM" class="container-fluid visible-xs hidden-md">
     <table cellspacing="8" cellpadding="0" border="0" id="Table_01">
         <tr>
-            <td> <input id="createTH_Btn" type="image" src="../../images/menu_buttons/flatcreate.png" width="90%" height="50%"> </td>
-            <td> <input id="startTH_Btn" type="image" src="../../images/menu_buttons/flatstart.png" width="90%" height="50%"> </td>
-            <td> <input id="profile_Btn" type="image" src="../../images/menu_buttons/flatprofile.png" width="90%" height="50%"> </td>
-            <td> <input id="help_Btn" type="image" src="../../images/menu_buttons/flathelp.png" width="90%" height="50%"> </td>
+            <td><input id="createTH_Btn" type="image" src="/goc/images/menu_buttons/flatcreate.png" width="90%"
+                       height="50%"></td>
+            <td><input id="startTH_Btn" type="image" src="/goc/images/menu_buttons/flatstart.png" width="90%"
+                       height="50%"></td>
+            <td><input id="profile_Btn" type="image" src="/goc/images/menu_buttons/flatprofile.png" width="90%"
+                       height="50%"></td>
+            <td><input id="help_Btn" type="image" src="/goc/images/menu_buttons/flathelp.png" width="90%" height="50%">
+            </td>
         </tr>
     </table>
 </div>
